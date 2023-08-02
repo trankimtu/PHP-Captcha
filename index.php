53ec69d5779668b02ce54a66ece4283d1072ba21
trankimtu <33793431+trankimtu@users.noreply.github.com>
2023-08-01 21:38:03 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-
+  session_start();
+if(isset($_POST["captcha_code"])){
+  if($_POST["captcha_code"] === $_SESSION["captcha_code"]){
+    $message = '<p class="text-success" id="msg">Message Submitted Successfully</p>';
+  }
+}
 ?>
